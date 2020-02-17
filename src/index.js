f503f643c57e0031fafc2bd406618bb582b7ca92
Mark <sipizork@gmail.com>
2020-02-17 21:08:28 +0100

    fourth-lesson

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import './css/style.css';
 import StorePicker from './components/StorePicker';
-import App from './components/App'
+import App from './components/App';
+import NotFound from './components/NotFound';
 
-render(<App />, document.querySelector('#root'));
+const Root = () => {
+  return(
+
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <StorePicker />
+          </Route>
+          <Route exact path="/store/:storeID">
+            <App />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
+  )
+}
+
+render(<Root />, document.querySelector('#root'));
+
+
+/* */
