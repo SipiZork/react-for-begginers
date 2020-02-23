950fae6ab69824a826e79b9b6010fa01007542ec
Mark <sipizork@gmail.com>
2020-02-23 13:09:33 +0100

    fix scrollbars

--- src/components/App.js
+++ src/components/App.js
@@ -97,17 +97,17 @@ class App extends React.Component {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header tagline="Fresh Seafood Market" />
-          <ul className="fishes">
-            {Object.keys(this.state.fishes).map(key => (
-              <Fish
-                key={key}
-                index={key}
-                details={this.state.fishes[key]}
-                addToOrder={this.addToOrder}
-              />
-            ))}
-          </ul>
+            <Header tagline="Fresh Seafood Market" />
+            <ul className="fishes">
+              {Object.keys(this.state.fishes).map(key => (
+                <Fish
+                  key={key}
+                  index={key}
+                  details={this.state.fishes[key]}
+                  addToOrder={this.addToOrder}
+                />
+              ))}
+            </ul>
         </div>
         <Order
           fishes={this.state.fishes}
